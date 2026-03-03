d776d130c5315a8ccc1e16bc671cc6aaa83c87b2
Zak Waddle <zakwaddle@gmail.com>
2026-03-02 23:30:03 -0500

    bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='action_manager',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/zakwaddle/action_manager',
     packages=find_packages(where="."),
     package_dir={"": "."},
